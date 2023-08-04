671f3b01ea9bdf51d550586ea618de200dfa577d
Simon Cropp <simon.cropp@gmail.com>
2023-08-04 18:57:43 +1000

    .

--- src/WinDebloat/RegistryJob.cs
+++ src/WinDebloat/RegistryJob.cs
@@ -32,16 +32,3 @@
         return Task.FromResult(new JobResult(true, null));
     }
 }
-
-public record WingetInstallJob(
-    string Name,
-    string? Notes = null):
-    IJob
-{
-    public string Description => $"Installing {Name}";
-
-    public Task Run()
-    {
-       return  WinGet.InstallByName(Name);
-    }
-}
---
+++ src/WinDebloat/WingetInstallJob.cs
@@ -0,0 +1,12 @@
+﻿public record WingetInstallJob(
+    string Name,
+    string? Notes = null):
+    IJob
+{
+    public string Description => $"Installing {Name}";
+
+    public Task Run()
+    {
+        return  WinGet.InstallByName(Name);
+    }
+}
