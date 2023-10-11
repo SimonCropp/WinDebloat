14a8bab5ee020f4c6b81aab54d03420b19bfba61
Benjamin Evenson <2031163+benjiro@users.noreply.github.com>
2023-10-12 09:15:59 +1000

    Improve winget version parsing logic
    Winget follows semver versioning so if the target system has
    a preview or canary build installed say "1.5.231-preview" the System.Version will fallover
    
    To handle this scenario just drop the prerelease tags from the semver
    version tag. The prerelease tag isn't really important for WinDebloat
    to enforce a min version.

---
+++ src/Tests/SafeVersionTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Tests;
+
+public class VersionExtensionsTests
+{
+    private static IEnumerable<TestCaseData> GetVersionTestData()
+    {
+        yield return new("v1.2.3", new Version(1, 2, 3));
+        yield return new("v1.2.3.4", new Version(1, 2, 3, 4));
+        yield return new("v1.2.3-prerelease", new Version(1, 2, 3));
+        yield return new("v1.2.3.4-prerelease", new Version(1, 2, 3, 4));
+    }
+
+    [TestCaseSource(nameof(GetVersionTestData))]
+    public void VersionParse(string input, Version expected)
+    {
+        var actual = SafeVersion.Parse(input);
+        Assert.AreEqual(expected, actual);
+    }
+}
