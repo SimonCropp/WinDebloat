e636839a7808d82a82fc9b6569d84945d296be6c
Simon Cropp <simon.cropp@gmail.com>
2023-10-11 15:29:06 +1100

    cleanup

--- src/WinDebloat/Jobs/RegistryKeyJob.cs
+++ src/WinDebloat/Jobs/RegistryKeyJob.cs
@@ -1,14 +1,8 @@
-﻿public record RegistryValueJob(
+public record RegistryKeyJob(
     string Key,
-    string Name,
-    object ApplyValue,
-    object? RevertValue,
-    RegistryValueKind Kind = RegistryValueKind.DWord,
     string? Notes = null) :
     IJob
 {
-    public string Path => @$"{ShortKey}\{Name}";
-
     public string ShortKey =>
         Key
             .Replace("HKEY_LOCAL_MACHINE", "HKLM")
@@ -16,4 +10,6 @@
             .Replace("HKEY_CLASSES_ROOT", "HKCR")
             .Replace("HKEY_CURRENT_USER", "HKCU")
             .Replace("HKEY_USERS", "HKU");
+
+    public string Name => Key.Split('\\').Last();
 }
