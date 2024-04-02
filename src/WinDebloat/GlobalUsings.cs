7f27c0be92d21897d693067d8be59a9df40aad16
Simon Cropp <simon.cropp@gmail.com>
2024-04-02 22:41:51 +1100

    refs and cleanup

--- src/WinDebloat/GlobalUsings.cs
+++ src/WinDebloat/GlobalUsings.cs
@@ -1,6 +1,4 @@
-﻿// Global using directives
-
-global using System.Diagnostics.CodeAnalysis;
+﻿global using System.Diagnostics.CodeAnalysis;
 global using CliWrap;
 global using Microsoft.Win32;
 global using Serilog;
